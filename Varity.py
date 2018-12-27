797413463267908f8abb121191e01e9a6ab03aab
Ann Golubeva <mrako142@mail.ru>
2018-12-27 14:09:47 +0300

    Create Varity.py

---
+++ Varity.py
@@ -0,0 +1,23 @@
+'''
+Разработка функции-декоратора, позволяющей выполнять
+декорируемую функцию единожды.
+'''
+
+def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+
+@run_once
+def my_function(foo, bar):
+    return foo/bar
+
+print(my_function(5, 10))
+print(my_function(5, 10))
+print(my_function(17, 17))
+print(my_function(5, 10))
+print(my_function(17, 17))
